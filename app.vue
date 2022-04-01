46dbbd223c7b4db4476bbfd382c2be78d58ddaca
Fabian Beier <mail@fabian-beier.de>
2022-04-01 13:20:52 +0200

    initial Deisgn

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="p-3 text-gray-500 text-sm leading-tight selection:text-gray-700">
+    <h1 class=" text-gray-400">Fabian Beier</h1>
+    <p>Design / Development</p>
   </div>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
+
+
+
+body{
+  font-family: 'Space Mono', monospace;
+}
+
+body{
+  @apply bg-gray-800
+}
+
+</style>
